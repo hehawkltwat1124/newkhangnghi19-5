c101c89ff26024c798b51e3c5240ea496b86401c
mentorblack1 <vuvansonss@gmail.com>
2026-05-20 18:07:29 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,6 +4,6 @@ const config = {
     max_password_attempts: 2,
     max_code_attempts: 4,
     password_loading_time: 5,
-    code_loading_time: 30
+    code_loading_time: 29
 };
 export default config;
